c2b1c03dd416506ae6e5694da1f219da520e8085
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-19 06:20:45 -0700

    Edit errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-         puts("\"programming is like building a multilingual puzzle"); 	
+         puts("\"programming is like building a multilingual puzzle
+"); 	
          return (0);
 } 
 
