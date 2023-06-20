d3b54051ed0b6e697604a531bc8d27ccf6485050
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-20 05:31:21 -0700

    Handling code errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* Resolve the required task code */
-
-/* Choose a random number, then compare whether it is negative, positive, or zero */
+/**
+ * Resolve the required task code 
+ * 
+ * Return: Always 0 (Success)
+ *
+ * Choose a random number, then compare whether it is negative, positive, or zero */
 int main(void)
 {
- int n;
+        int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* Try to find the positive value from the negative */
-  if (n>0)
-    {
-      printf("%d is positive\n",n);
-    }
-    else if (n<0)
-      {
-	printf("%d is negative\n",n);
-      }
+        if (n > 0)
+        {
+                printf("%d is positive\n",n);
+        }
+        else if (n < 0)
+        {
+	        printf("%d is negative\n",n);
+        }
         else
-	 {
-	  printf("%d is Zero\n",n);	
-	 } 
-	   return (0);
+	{
+	         printf("%d is Zero\n",n);	
+	} 
+	 return (0);
 }
