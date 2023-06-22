9d1e889141bbe87ee8bb6ac9886ce7b3cd2e4572
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-22 06:19:30 -0700

    done all

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ /**
+  * main - will call the function that prints the characters 
+  *
+  * Return: Always 0.
+  */
+void print_alphabet(void)
+{
+ printf("abcdefghijklmnopqrstuvwxyz\n");
+  
+}
+int main(void)
+{
+ print_alphabet();
+ return(0);
+}
+
