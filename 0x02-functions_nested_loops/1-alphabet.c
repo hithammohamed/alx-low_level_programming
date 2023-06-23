60ddf1ea6b5e9e7cbedef92dec8e01ff9621ff03
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-23 06:55:26 -0700

    A file that prints the required characters has been created

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Task from ALX
+ *
+ * This code prints the letters of the English alphabet.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int words;
+
+	for (words = 'a'; words <= 'Z'; words++)
+		_putchar(words);
+	_putchar('\n');
+}
