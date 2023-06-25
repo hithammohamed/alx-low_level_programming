2e590b8fba360c87bb58dcc6a6547cbdb3497365
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-25 06:39:25 -0700

    The code detects the number between 0 and 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - The code detects the c that falls between the required range
+ *
+ * @c: input for numbers
+ *
+ * Return: 1 if its number between 0 to 9, 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
