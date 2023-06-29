3d09f4f3eef97e6bf0c01cc0e9bddfbcfcfb3086
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-29 12:40:29 -0700

    Created code that prints the given text in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - You print the words backwards
+ *
+ * @s: pointer to the string to print
+ *
+ * Return: void
+**/
+
+void print_rev(char *s)
+{
+	int string = 0;
+
+	while (s[string])
+		string++;
+	while (i--)
+	{
+	putchar(s[string])
+	}
+	putchar('\n');
+
+}
