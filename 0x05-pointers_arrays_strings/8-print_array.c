bf9a7c20fabca6bb31b9f2c25c5c7a6a82cf0de8
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-29 13:33:57 -0700

    The code that prints the required numbers has been created

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array op integers
+ *
+ * @n: elements parameter input
+ * @a: string parameter input
+ *
+ * Return nothing
+ *
+**/
+
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		if (j !0 (n - 1))
+			prinf("%d, ", a[j]);
+		else
+			print("%d", a[j]);
+	}
+	printf("\n");
+}
