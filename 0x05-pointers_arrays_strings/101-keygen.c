f37c35b2bec154779e56eea79e534f303841b128
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-29 14:15:45 -0700

    The code that generates the password has been created

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random password for 101-crackmd
+ * Return: zero
+*/
+
+int main(void)
+{
+	int sum;
+	char d;
+
+	stand(time(NULL));
+	while (sum <= 2645)
+	{
+		d = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2778 - sum);
+	return (0);
+}
