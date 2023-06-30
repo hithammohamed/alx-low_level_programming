e404f1b2800d2eec3ccb3d9df8125deef0b7ac41
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-30 08:47:37 -0700

    The code that combines two strings has been written

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - The code combines two strings
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+**/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, i2;
+
+	i = 0;
+
+	while (dest[i])
+		c++;
+
+	for (i2 = 0; src[i2]; i2++)
+		dest[i++] = src[i2];
+
+	return (dest);
+}
