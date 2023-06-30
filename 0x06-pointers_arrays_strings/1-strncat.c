c08ae42543ef1d9f22809c5d0443a6924444da7b
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-30 09:07:17 -0700

    The code that combines two strings has been written

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,7 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; a < n && src[a] != '\0'; a++)
 		dest[m + a] = src[a];
 
-	dest[m + i] = '\0';
+	dest[m + a] = '\0';
 
 	return (dest);
 }
