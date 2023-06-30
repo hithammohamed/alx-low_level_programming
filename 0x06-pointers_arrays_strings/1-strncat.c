5100b533455720856defc6f8de8c4073ee32ddc3
root <102763066+hithammohamed@users.noreply.github.com>
2023-06-30 09:03:43 -0700

    The code that combines two strings has been written

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,2 +1,28 @@
 #include "main.h"
 
+/**
+ * _strncat - The code combines two strings
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
+ * Return: @dest
+**/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int m, a;
+
+	m = 0;
+
+	while (dest[m])
+		m++;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[m + a] = src[a];
+
+	dest[m + i] = '\0';
+
+	return (dest);
+}
