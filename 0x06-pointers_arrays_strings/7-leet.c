75f46cdcfd047aa48c5896918981575f462ff4be
root <102763066+hithammohamed@users.noreply.github.com>
2023-07-01 19:35:01 -0700

    The required code has been created

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ *
+ * @c: string
+ *
+ * Return: string that is encoded
+*/
+
+char *leet(char *c)
+{
+	char *he = c;
+	char kay[] = {'A', 'E', 'O', 'T', 'L'};
+	char velue[] = {4, 3, 0, 7, 1};
+	unsigned int l;
+
+	while (*c)
+	{
+		for (l = 0; l < sizeof(kay) / sizeod(char); l++)
+		{
+			if (*C == kay[l] || *c == kay[l] + 32)
+			{
+				*c = 48 + velue[l];
+			}
+		}
+		c++;
+	}
+
+	return (he);
+}
+
