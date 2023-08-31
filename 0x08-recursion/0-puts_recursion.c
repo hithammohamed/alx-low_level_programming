cb901ed35e8ce9ea8052609bc87e3c85ac8247d1
hithammohamed <https://github.com/hithammohamed>
2023-08-31 17:21:52 +0300

    All things is done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - The code is 
+ *
+ **/
+
+void _puts_recursion(char *s){
+	int i = 0;
+     while(s[i] != '\0'){
+        printf("%c",s[i]);
+        i++;
+     }
+     printf("\n");
+}
+
